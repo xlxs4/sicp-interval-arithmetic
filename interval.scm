adf3aa403f203a9b2d7f3b0895cccbc3d545f276
xlxs4 <orousoultzoglou@gmail.com>
2023-03-05 19:53:52 +0200

    Center + percentage tolerance constructor

--- interval.scm
+++ interval.scm
@@ -66,3 +66,21 @@
                       (make-interval
                        (/ 1.0 yu)
                        (/ 1.0 yl))))))
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i)
+        (upper-bound i))
+     2))
+
+(define (make-center-percent c p)
+  (let ((width (abs (* (/ p 100) c))))
+    (make-interval (- c width) (+ c width))))
+
+(define (percent i)
+  (if (= (center i) 0)
+      0
+      (* 100 (abs (/ (- (upper-bound i) (center i))
+                     (center i))))))
